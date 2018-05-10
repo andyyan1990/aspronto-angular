eb861b03990ba0737c2f6eaf8c694de7bf280e76
Andy Yan <xyan0002@student.monash.edu>
2018-05-10 19:37:18 +1000

    typo for saving resource
    shall be changed back before submission

--- src/app/forecast.service.ts
+++ src/app/forecast.service.ts
@@ -7,7 +7,7 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ForecastService {
 
-  forecastUrl = "https://dataservice.accuweather.com/forecasts/v1/daily/5day/26216?apikey=IkbSjMGrrfvIfR8kbvLAJfaoaJwureAa&language=en&details=false&metric=true";
+  forecastUrl = "https://dataservic.accuweather.com/forecasts/v1/daily/5day/26216?apikey=IkbSjMGrrfvIfR8kbvLAJfaoaJwureAa&language=en&details=false&metric=true";
 
   constructor(private http:HttpClient) { }
 
