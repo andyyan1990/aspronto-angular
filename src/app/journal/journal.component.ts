c7be28d902a6508b9e501874a150b0c66526ba25
Jialin <jyan247@student.monash.edu>
2018-05-17 22:56:13 +1000

    updateLocation
    update Location by using location object

--- src/app/journal/journal.component.ts
+++ src/app/journal/journal.component.ts
@@ -39,8 +39,10 @@ export class JournalComponent implements OnInit {
   getJournal = false;
   servers = [];
   showAddButton = false;
+  userLocation: object;
 
-  constructor(private serverService: ServerService, private shareData: ShareDataService,
+  constructor(private serverService: ServerService,
+    private shareData: ShareDataService,
     private weatherService: WeatherService,
     private heroku: HerokuDataModelService,
     private route : ActivatedRoute,
@@ -48,12 +50,9 @@ export class JournalComponent implements OnInit {
     private googleAPI: GoogleMapService) { }
 
   ngOnInit() {
-    navigator.geolocation.getCurrentPosition(position => {
-      this.displayLocationInfo(position),
-        console.log("get the object position:" + position);
-    })
 
-
+    this.shareData.currentLocationMessage.subscribe(message => this.userLocation = message);
+    // console.log("location message jialin: "+this.userLocation); 
     this.shareData.loginedUserMessage.subscribe(message => this.loginedUser = message)
     //console.log(this.loginedUser)
     this.test = this.serverService.getUser(this.loginedUser);
@@ -86,23 +85,6 @@ export class JournalComponent implements OnInit {
   }
 
 
-  displayLocationInfo(position) {
-    this.currentLocation = position
-    const lng = position.coords.longitude;
-    const lat = position.coords.latitude;
-    //console.log(`longitude: ${lng} | latitude: ${lat}`);
-    var requestUrl_pre = "https://maps.googleapis.com/maps/api/geocode/json?address=";
-    var requestUrl_suf = "&key=key=AIzaSyBD-eQhzhZi0hh8NaHA_0g5CPgOVl9M82Q";
-    this.http.get(requestUrl_pre + lat + "," + lng + requestUrl_suf).subscribe(locationInfo => {
-      if (locationInfo['status'] == 'OK') {
-        this.currentLocation = locationInfo['results'][0]['address_components'][1]['long_name']
-        console.log(this.currentLocation)
-      } else {
-        this.currentLocation = 'Melbourne'
-      }
-      this.shareData.changeCurrentLocation(this.currentLocation);
-    });
-  }
 
   onAddJournal(){
     this.test = this.serverService.getUser(this.loginedUser);
@@ -113,7 +95,7 @@ export class JournalComponent implements OnInit {
     this.dnt = this.dnt.slice(0,25);
     // console.log(this.dnt);
     // console.log(this.dataToBeShared);
-    this.servers.push({date: this.dnt, risk: this.riskLevelText,condition: this.dataToBeShared['condition']['text'], humidity: this.dataToBeShared['humidity'], pressure: this.dataToBeShared['pressure_mb'], temperature: this.dataToBeShared['temp_c'], windDirection: this.dataToBeShared['wind_dir'], windSpeed:this.dataToBeShared['wind_kph'],location: this.currentLocation});
+    this.servers.push({date: this.dnt, risk: this.riskLevelText,condition: this.dataToBeShared['condition']['text'], humidity: this.dataToBeShared['humidity'], pressure: this.dataToBeShared['pressure_mb'], temperature: this.dataToBeShared['temp_c'], windDirection: this.dataToBeShared['wind_dir'], windSpeed:this.dataToBeShared['wind_kph'],location: this.userLocation});
     this.serverService.storeServers(this.servers)
     .subscribe(
       (response) => console.log(response),
